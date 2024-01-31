d5f775be96a74b583b68bc1977df223a8870da97
James Hodgkinson <james@terminaloutcomes.com>
2024-01-31 13:21:49 +1000

    build script fix

--- build-ui.sh
+++ build-ui.sh
@@ -3,4 +3,5 @@
 cd ui && yarn run setup
 cd ..
 
+rm -f splunk_add_on_ucc_framework/package/appserver/static/js/build/*
 cp -n -R ui/dist/build/* splunk_add_on_ucc_framework/package/appserver/static/js/build/
